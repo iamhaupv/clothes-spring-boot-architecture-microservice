be103f0c0e8902517fb8e1dd476db3ca115b8673
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-16 13:02:59 +0700

    up clothes service

---
+++ clothes-service/src/main/java/com/example/clothes_service/ClothesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesServiceApplication.class, args);
+	}
+
+}
---
+++ clothes-service/src/test/java/com/example/clothes_service/ClothesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
